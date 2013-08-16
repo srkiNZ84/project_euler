a641ebe7d1f591a7c3910b6057c3abc5ff402c20
Srdan Dukic <srdan.dukic@gmail.com>
2013-08-16 21:30:47 +1200

    Added code to read in names.

--- problem_22/Problem22.java
+++ problem_22/Problem22.java
@@ -1,10 +1,26 @@
+import java.io.*;
+
 public class Problem22{
 
 	public Problem22(){
+		try{
+			BufferedReader br = new BufferedReader(new FileReader("names.txt"));
+			String line = br.readLine();
 
+			System.out.println("Line was: " + line);
+
+			String[] names = line.split(",");
+			for(int i = 0; i < names.length; i++){
+				names[i] = names[i].replace("\"", "");
+				System.out.println("Name: " + names[i]);
+			}
+		}
+		catch(Exception ex){
+			System.out.println(ex);
+		}
 	}
 
-	public static main(String[] args){
+	public static void main(String[] args){
 		Problem22 p22 = new Problem22();
 	}
 }
