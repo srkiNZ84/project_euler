3a2aecfaeb48f988a9358d56ce831edd444f9dd3
srkiNZ84 <srdan.dukic@gmail.com>
2013-08-20 10:02:21 +1200

    Added partial solution file to the repository. Kind of pointless as it will be merged out by the complete solution from GitHub.

--- problem_22/Problem22.java
+++ problem_22/Problem22.java
@@ -1,10 +1,51 @@
+import java.io.*;
+
 public class Problem22{
 
 	public Problem22(){
-
+		try{
+			String currentLine;
+                        BufferedReader br = new BufferedReader(new FileReader("names.txt"));
+                        String[] namesArray;
+                        
+                        while((currentLine = br.readLine()) != null){
+				namesArray = currentLine.split(",");
+				//System.out.println("There are a total of " + temp.length + " names.");
+                                for(int i = 0; i < namesArray.length; i++){
+                                        namesArray[i] = stripQuotes(namesArray[i]);
+                                }
+			}
+			
+			// TODO: Sort the list
+			String[] orderedNamesArray = orderNames(namesArray);
+		}
+		catch(Exception ex){
+			ex.printStackTrace();
+		}
+	}
+	
+	private String[] orderNames(String[] namesArray)
+		int pivot = namesArray.length();
+		
+		for(int i = 0; i < namesArray.length(); i++){
+			if(namesArray[i].compareTo(namesArray[pivot]) < 0){
+				// the string being compared is less and should go "in front" of the pivot
+			}
+			else if(namesArray[i].comparedTo(namesArray[pivot]) > 0){
+				// the string being compared is greater and should go "behind" the pivot
+			}
+			else{
+				// the string being compared is the same. Nothing to be done
+			}
+		}
+	}
+	
+	private String stripQuotes(String name){
+		name = name.substring(1, name.length() -1);
+		return name;
 	}
 
-	public static main(String[] args){
+	public static void main(String[] args){
 		Problem22 p22 = new Problem22();
 	}
 }
